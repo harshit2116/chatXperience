727fe9b8d179db3ce507f2110b6603edad8f06b3
angerion116 <evu.angerion6@gmail.com>
2023-11-05 08:26:38 +0530

    Add Link component to dashboard tools

--- app/(main)/(routes)/dashboard/page.tsx
+++ app/(main)/(routes)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { MessageSquare, ArrowRight, ImageIcon, VideoIcon, Code, Settings, Messag
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import Link from 'next/link';
 
 const tools = [
   {
@@ -41,7 +42,7 @@ const tools = [
     icon: MessagesSquare,
     color: "text-blue-700",
     bgcolor: "bg-blue-700/10",
-    href: "/servers/channels",
+    href: "/servers/channels"
   },
 
   {
@@ -56,6 +57,7 @@ const tools = [
 
 const DashboardPage = () => {
   const router = useRouter();
+
   return (
     <div> 
         <div className="mb-8 space-y-4">
@@ -66,21 +68,21 @@ const DashboardPage = () => {
 
             <div className="px-4 md:px-20 lg:px-32 space-y-4">
               {tools.map((tool) => (
-                <Card 
-                onClick = {() => router.push(tool.href)}
-                 key={tool.href} 
-                 className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
-
-                  <div className="flex items-center gap-x-4">
-                    <div className={cn("p-2 w-fit rounded-md", tool.bgcolor)}>
-                      <tool.icon className={cn("w-8 h-8", tool.color)} />
+                <Link href={tool.href} key={tool.href}>
+                  <Card 
+                    className="p-4 border-black/5 bg-white dark:bg-[#2d1455] flex items-center justify-between hover:shadow-md transition cursor-pointer"
+                  >
+                    <div className="flex items-center gap-x-4">
+                      <div className={cn("p-2 w-fit rounded-md", tool.bgcolor)}>
+                        <tool.icon className={cn("w-8 h-8", tool.color)} />
+                      </div>
+                      <div className="font-semibold">
+                        {tool.label}
+                      </div>
                     </div>
-                    <div className="font-semibold">
-                      {tool.label}
-                    </div>
-                  </div>
-                  <ArrowRight className="w-5 h-5" />
-                 </Card>
+                    <ArrowRight className="w-5 h-5" />
+                  </Card>
+                </Link>
               ))}
             </div>
         </div>
