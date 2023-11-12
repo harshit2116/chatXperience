906ecbab2cfb3ce808ad03b98f416eb48ce4a971
angerion116 <evu.angerion6@gmail.com>
2023-11-12 16:05:43 +0530

    Risky Commit

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,19 +1,31 @@
 import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface HeadingProps {
-    title: string;
-    description: string;
-    icon: LucideIcon;
-    iconColor?: string;
-    bgcolor?: string;
+  title: string;
+  description: string;
+  icon: LucideIcon;
+  iconColor?: string;
+  bgColor?: string;
 }
 
 export const Heading = ({
   title,
   description,
-  icon,
+  icon: Icon,
   iconColor,
-  bgcolor,
+  bgColor,
 }: HeadingProps) => {
-  return <div>Heading Component</div>;
+  return (
+      <div className="px-4 lg:px-8 flex items-center gap-x-3  mb-8">
+        <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+          <Icon className={cn("w-10 h-10", iconColor)} />
+        </div>
+
+        <div>
+          <h2 className="text-3xl 1f1f1f"> {title} </h2>
+          <p className="text-sm text-muted-foreground"> {description} </p>
+        </div>
+      </div>
+  );
 };
