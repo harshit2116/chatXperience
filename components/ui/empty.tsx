e14f14333db12cf0fd6c7d4642fe6cc17bf02e6a
angerion116 <evu.angerion6@gmail.com>
2023-11-19 16:27:35 +0530

    Update empty.png to emptyconversation.png in Empty component

--- components/ui/empty.tsx
+++ components/ui/empty.tsx
@@ -11,7 +11,7 @@ export const Empty = ({
   return (
     <div className="h-full p-20 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
-        <Image src="/empty.png" fill alt="Empty" />
+        <Image src="/emptyconversation.png" fill alt="Empty" />
       </div>
       <p className="text-muted-foreground text-sm text-center">
         {label}
