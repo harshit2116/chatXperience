c69f6ccfb427786710f6df82ef3890349cede429
Harshit Saini <77571504+harshit2116@users.noreply.github.com>
2023-11-19 16:11:42 +0530

    Update redirect-server.tsx

--- components/redirect-server.tsx
+++ components/redirect-server.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export const RedirectServer = () => {
   useEffect(() => {
-    window.location.href = "https://chat-xperience-swpt-git-master-harshit2116s-projects.vercel.app/invite/43e7065f-d80f-482d-82af-075b8ed51e5e";
+    window.location.href = "https://chatxperience-production.up.railway.app/invite/43e7065f-d80f-482d-82af-075b8ed51e5e";
   }, []);
 
   return null;
