95b5ba638a7d78fa0dca35adf01b160ecbc4527f
angerion116 <evu.angerion6@gmail.com>
2023-11-22 07:46:37 +0530

    Update loader component with new image

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-      <div className="w-10 h-10 relative animate-spin">
-        <Image alt="Logo" src="/logo.png" fill />
+      <div className="w-16 h-16 relative">
+        <Image alt="Logo" src="/loader.gif" fill />
       </div>
       <p className="text-sm text-muted-foreground">
         With this I summon the power of AI...
